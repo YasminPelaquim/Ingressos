c2e28f784874635b585ed118dd3c787b741efeae
yasmi <yasmi@YASMIN>
2024-04-21 20:37:44 -0300

    pronto

--- Ingressos/Program.cs
+++ Ingressos/Program.cs
@@ -15,65 +15,58 @@ namespace Ingressos
             int valti = 0;
             int valt = 0;
             
-            for(int i = 1; i <= 30;  i++)
+            for(int i = 1; i <= 5;  i++)
             {
                 Console.WriteLine("Meia entrada de estudante? Sim/Não ");
                 string resp = Console.ReadLine();
-                if (resp == "s" && i <= 10)
+                if (resp == "s" && i <= 5)
                 {
-                    int val = 100 / 2;
+                    int val = 50;
                     Console.WriteLine("O valor a ser pago é de " + val);
                     valtm++;
                     valt += val;
-                } else if (i <= 10)
+                } else if (resp == "s" && i <= 20)
+                    {
+                        int val = 75;
+                        Console.WriteLine("O valor a ser pago é de " + val);
+                        valtm++;
+                        valt += val;
+                        
+                     } else if (resp == "s" && i <= 30)
+                    {
+                        int val = 100;
+                        Console.WriteLine("O valor a ser pago é de " + val);
+                        valtm++;
+                        valt += val;
+                    }else if (i <= 10)
                     {
                         int val = 100;
                         Console.WriteLine("O valor a ser pago é de " + val);
                         valti++;
                         valt += val;
-                        
-                    }
-                    if (i <= 20)
-                    {
-                        int val = 150 / 2;
-                        Console.WriteLine("O valor a ser pago é de " + val);
-                        valtm++;
-                        valt += val;
-                    }
-                    if (i <= 30)
-                    {
-                        int val = 200 / 2;
-                        Console.WriteLine("O valor a ser pago é de " + val);
-                        valtm++;
-                        valt += val;
-                    }
-                } else if (i <= 10)
-                {
-                    int val = 100;
-                    Console.WriteLine("O valor a ser pago é de " + val);
-                    valti++;
-                    valt += val;
-                } else if(i <= 20)
+                    
+                } else if (i <= 20)
                 {
                     int val = 150;
                     Console.WriteLine("O valor a ser pago é de " + val);
                     valti++;
                     valt += val;
-                } else
+                } else if(i <= 30)
                 {
                     int val = 200;
                     Console.WriteLine("O valor a ser pago é de " + val);
                     valti++;
                     valt += val;
-                }
+                } 
 
                 Console.ReadKey();
             }
-           // Refazer os if com && e colocar se é estudante e o lote 
 
             Console.WriteLine("O faturamento total é de " + valt);
             Console.WriteLine("A quantidade de meios-ingressos pagos foi de " + valtm);
             Console.WriteLine("A quantia de ingressos inteiros foi de " + valti);
+
+            Console.ReadKey();
         }
         
     }
